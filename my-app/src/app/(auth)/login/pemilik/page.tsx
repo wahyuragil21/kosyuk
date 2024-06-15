965b2d9dc0698c4ce1ee43cfe044c985a098feb2
Wahyu Ragil <wahyu.ragil@phiraka.com>
2024-06-15 20:09:06 +0700

    Feat : Create UI for Login pemilik dan pencari kost

---
+++ my-app/src/app/(auth)/login/pemilik/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import FormLogin from "@/components/formLogin";
+import NavbarProduct from "@/components/navbar";
+
+export default function pemilik() {
+  return (
+    <>
+      <NavbarProduct />
+      <FormLogin />
+    </>
+  );
+}
