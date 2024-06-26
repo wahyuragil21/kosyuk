c0d391d3c6237d947c64703c5ac3bdf95f52679d
Wahyu Ragil <wahyu.ragil@phiraka.com>
2024-06-26 10:48:17 +0700

    Feat : add config for link image

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -3,6 +3,9 @@
  */
 const nextConfig = {
   /* config options here */
+  images: {
+    domains: ['static.nike.com', 'i.pinimg.com'], // Tambahkan domain gambar Anda di sini
+  },
 }
 
 export default nextConfig
